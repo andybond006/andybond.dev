41908885c470249367cb3ed4dc3ec90039c79fc9
Andy Bond <andybond006@gmail.com>
2021-04-25 19:57:33 -0400

    feat: improve navbar

--- postcss.config.js
+++ postcss.config.js
@@ -4,10 +4,10 @@ module.exports = {
     require(`autoprefixer`),
     ...(process.env.NODE_ENV === "production"
       ? [
-          require(`cssnano`)({
-            preset: "default",
-          }),
-        ]
+        require(`cssnano`)({
+          preset: "default",
+        }),
+      ]
       : []),
   ],
 };
