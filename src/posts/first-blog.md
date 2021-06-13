24031a9cb9c71b83865131939ccdfeeddad7db4d
Andy Bond <andybond006@gmail.com>
2021-06-13 11:26:14 -0400

    feat: clean up

---
+++ src/posts/first-blog.md
@@ -0,0 +1,12 @@
+---
+title: First Blog
+description: Test Blog
+author: Andy Bond
+date: 2021-04-30T00:55:05.020Z
+type: personal
+tags:
+  - life
+---
+# Test
+
+## Test 2
---
+++ src/static/js/theme.js
@@ -0,0 +1,50 @@
+function storageAvailable(type) {
+  var storage;
+  try {
+    storage = window[type];
+    var x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      // everything except Firefox
+      e.code === 22 ||
+      // Firefox
+      e.code === 1014 ||
+      // test name field too, because code might not be present
+      // everything except Firefox
+      e.name === 'QuotaExceededError' ||
+      // Firefox
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      (storage && storage.length !== 0);
+  }
+}
+
+const themeKey = 'andybond.dev-theme';
+const available = storageAvailable('localStorage');
+const system = window.matchMedia('(prefers-color-scheme: dark)').matches;
+let dark = system;
+
+if (available) {
+  dark = localStorage.getItem(themeKey) === 'dark';
+}
+
+if (dark) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+
+if (available) {
+  localStorage.setItem(themeKey, dark ? 'dark' : 'light');
+}
+
+function toggleTheme() {
+  document.documentElement.classList.toggle('dark');
+  if (available) {
+    const current = localStorage.getItem(themeKey);
+    localStorage.setItem(themeKey, current === 'dark' ? 'light' : 'dark');
+  }
+}
